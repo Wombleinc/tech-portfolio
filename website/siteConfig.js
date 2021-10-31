9c7c28352006bfd57a2353923fd1ee4f137776b6
WombleInc <joshkw10691@gmail.com>
2021-10-31 13:22:09 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Josh Womble', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
-    profilePhoto: '../docs/assests/wombleinc.jpg',
+    profilePhoto: '../docs/assets/wombleinc.jpg',
     url: 'https://Wombleinc.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
